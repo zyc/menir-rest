105347b8462e57b7551ed68e9bf5b189eefa4d8e
quarkusio <no-reply@quarkus.io>
2020-11-13 17:13:44 +0000

    Initial commit

---
+++ src/test/kotlin/org/acme/resteasy/ExampleResourceTest.kt
@@ -0,0 +1,20 @@
+package org.acme.resteasy
+
+import io.quarkus.test.junit.QuarkusTest
+import io.restassured.RestAssured.given
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.jupiter.api.Test
+
+@QuarkusTest
+class ExampleResourceTest {
+
+    @Test
+    fun testHelloEndpoint() {
+        given()
+          .`when`().get("/resteasy/hello")
+          .then()
+             .statusCode(200)
+             .body(`is`("hello"))
+    }
+
+}
